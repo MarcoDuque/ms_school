96503fc65cab10f5b1bba6fe504939692c340331
MarcoDuque <marcosduquesomatic@gmail.com>
2024-09-10 03:07:10 -0400

    basic crud

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import school_get_all, create_school
+from .views import school_get_all, create_school, school_find_byId, school_update, school_delete
 
 urlpatterns = [
+    path('school-create/', create_school, name='create_school'),
     path('school-get-all/', school_get_all, name='get_schools'),
-    path('school-create/', create_school, name='create_school')
+    path('school-get-by/<int:id>', school_find_byId , name='get_schools_byId'),
+    path('school-update-by/<int:id>', school_update , name='update_school'),
+    path('school-delete-by/<int:id>', school_delete , name='delete_schools')
 ]
