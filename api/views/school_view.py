a60d4cb701edb49adc6e00c0086a243afec674ea
MarcoDuque <marcosduquesomatic@gmail.com>
2024-09-16 01:23:26 -0400

    implements html view

--- api/views/school_view.py
+++ api/views/school_view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from rest_framework import viewsets
 from ..models.school import School
 from ..serializers.school_serializer import SchoolSerializer
@@ -5,3 +6,8 @@ from ..serializers.school_serializer import SchoolSerializer
 class SchoolViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolSerializer
     queryset = School.objects.all()
+    
+    def list(self, request, *args, **kwargs):
+        if request.accepted_renderer.format == 'api':
+            return render(request, 'school_list.html', {'schools': self.get_queryset()})
+        return super(SchoolViewSet, self).list(request, *args, **kwargs)
